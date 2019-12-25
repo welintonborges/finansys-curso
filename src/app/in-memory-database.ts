ac2ac410aab55ca6a4cd9e787eb895ed7466b842
welinton <welinton.b.borges@gmail.com>
2019-12-24 22:03:42 -0300

    implementar model e service da categoria

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,8 +1,9 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Category } from './pages/categories/shared/category.model';
 
 export class InMemoryDatabase implements InMemoryDbService{
     createDb(){
-        const categories =[
+        const categories:  Category[] = [
             { id: 1, nome:'Moradia' , description: 'Pagamento de Conta da Casa' },
             { id: 2, nome:'Saúde' , description: 'Plano de Saúde' },
             { id: 3, nome:'Lazer' , description: 'Cinema , parque' },
---
+++ src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Category } from './category.model';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private apiPath:string = "api/categories"
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Category[]>{
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategories)
+    )
+  }
+
+  getById(id: number): Observable<Category>{
+    const url = '${this.apiPath}/${id}';
+
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    )
+  }
+  
+  private jsonDataToCategories(jsonData: any[]): Category[]{
+    const categories: Category[]=[];
+    jsonData.forEach(element => categories.push(element as Category));
+    return categories;
+  }
+  
+  private jsonDataToCategory(jsonData: any): Category{
+    return jsonData as Category;
+  }
+
+  create(category: Category): Observable<Category>{
+    return this.http.post(this.apiPath, category).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    )
+  }
+
+  update(category: Category): Observable<Category>{
+    const url = '${this.apiPath}/${id}';
+    return this.http.put(url, category).pipe(
+      catchError(this.handleError),
+      map(() => category)
+    )
+  }
+
+  delete(id: number): Observable<Category>{
+    const url = '${this.apiPath}/${id}';
+
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+       map(() => null)
+    )
+  }
+
+  private handleError(error: any): Observable<any>{
+    console.log("ERRO NA REQUISIÇÃO ->", error);
+    return throwError(error);
+  }
+
+}
