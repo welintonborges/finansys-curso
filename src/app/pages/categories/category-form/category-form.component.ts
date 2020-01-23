6b7dcc79c5a5d4a6cd893d9d3422c3702ac28434
welinton <welinton.b.borges@gmail.com>
2020-01-23 01:49:31 -0300

    ajuste na alteração de dados

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -106,7 +106,7 @@ export class CategoryFormComponent implements OnInit , AfterContentChecked {
       }
 
       private updateCategory() {
-        const category: Category = Object.assign(new Category(), this.category);
+        const category: Category = Object.assign(new Category(), this.categoryForm.value);
 
         this.categoryService.update(category)
         .subscribe(
